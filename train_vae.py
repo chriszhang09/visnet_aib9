69f48eebb251a1318ee0af176bc58fb267b4a929
Chris Zhang <chriszhang@Chriss-MacBook-Pro-2.local>
2025-09-30 18:16:56 -0400

    bug fix

--- train_vae.py
+++ train_vae.py
@@ -28,7 +28,7 @@ import wandb
 from aib9_lib import aib9_tools as aib9
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from torch.amp import autocast, GradScaler
+from torch.cuda.amp import autocast, GradScaler
 
 
 def visualize_molecule_3d(coords, atomic_numbers, title="Molecule"):
